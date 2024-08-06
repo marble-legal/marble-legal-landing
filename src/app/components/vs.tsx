98d6999d543a29da919be9257d3f7c36cf349e42
Ramtin Rahmati <ramtin.rahmati79@gmail.com>
2024-08-06 10:04:02 +0300

    Update vs.tsx

--- src/app/components/vs.tsx
+++ src/app/components/vs.tsx
@@ -30,10 +30,10 @@ export default function VS() {
           Save on legal costs with Marble
         </p>
       </Animated>
-      <div className="flex flex-row max-md:flex-wrap md:gap-8 gap-4 w-full">
+      <div className="flex flex-row max-md:flex-wrap md:gap-8 gap-4 w-full items-stretch">
         <Animated
           variant="moveFromBottom"
-          className="bg-[#FFF8F7] rounded-[16px] w-full relative py-[4.625rem] px-[2.75rem] items-center justify-center flex flex-col gap-[1.4375rem]"
+          className="bg-[#FFF8F7] rounded-[16px] w-full relative py-[4.625rem] px-[2.75rem] flex flex-col gap-[1.4375rem] justify-center items-center"
         >
           <Image
             src="/red-hue.svg"
@@ -46,7 +46,7 @@ export default function VS() {
             priority
           />
 
-          <div className="z-[2] flex flex-col gap-[1.4375rem]">
+          <div className="z-[2] flex flex-col gap-[1.4375rem] items-center">
             <Image
               src="/lawyer.svg"
               alt="lawyer"
@@ -62,36 +62,37 @@ export default function VS() {
             >
               Lawyer
             </h4>
-            <ul className="flex flex-col gap-5 md:text-[1.25rem] text-base text-[#0A100B] items-center [&_ol]:text-center [&_ol]:leading-[140%]">
-              <ol className={outfit.className}>
+            <ul className="flex flex-col gap-5 md:text-[1.25rem] text-base text-[#0A100B] items-center text-center leading-[140%]">
+              <li className={outfit.className}>
                 Average hourly rate:{" "}
                 <span className="text-[#B85042] text-[1.125rem] font-bold">
                   $327/h
                 </span>
-              </ol>
-              <ol className={outfit.className}>
+              </li>
+              <li className={outfit.className}>
                 Average time spent on legal issues: <br className="md:hidden" />
                 <span className="text-[#B85042] text-[1.125rem] font-bold">
                   5-7 hours
                 </span>
-              </ol>
-              <ol className={outfit.className}>
+              </li>
+              <li className={outfit.className}>
                 Average cost for contract drafting:{" "}
                 <span className="text-[#B85042] text-[1.125rem] font-bold">
                   $200-$800
                 </span>
-              </ol>
+              </li>
             </ul>
           </div>
         </Animated>
+
         <Animated
           variant="moveFromBottom"
           delay={0.25}
-          className="bg-[#F0FFF0] rounded-[16px] w-full relative py-[4.625rem] px-[2.75rem] items-center justify-center flex flex-col gap-[1.4375rem] self-start h-full"
+          className="bg-[#F0FFF0] rounded-[16px] w-full relative py-[4.625rem] px-[2.75rem] flex flex-col gap-[1.4375rem] justify-center items-center content-start"
         >
           <Image
             src="/green-hue.svg"
-            alt="red-hue"
+            alt="green-hue"
             layout="intrinsic"
             width={1920}
             height={1080}
@@ -100,7 +101,7 @@ export default function VS() {
             priority
           />
 
-          <div className="z-[2] flex flex-col gap-[1.4375rem]">
+          <div className="z-[2] flex flex-col gap-[1.4375rem] items-center">
             <Image
               src="/marble.svg"
               alt="marble"
