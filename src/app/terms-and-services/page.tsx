1fd5b422f9be8c9a3c7a04e26ead2d15015bf0ef
Amit Patel <amit@micro1.ai>
2024-08-30 20:52:28 +0530

    Fix spacing

--- src/app/terms-and-services/page.tsx
+++ src/app/terms-and-services/page.tsx
@@ -10,13 +10,13 @@ export default function TermsAndServices() {
   return (
     <main className="flex flex-col items-center justify-between">
       <Navbar />
-      <div className="relative border-t border-t-[#E1ECE0] border-b border-b-[#D7D7D7] w-full flex flex-col items-center justify-center py-10 md:pt-[110px] md:pb-[150px]">
+      <div className="relative border-t border-t-[#E1ECE0] border-b border-b-[#D7D7D7] w-full flex flex-col items-center justify-center py-10 md:py-[110px]">
         <h1
           className={`text-2xl md:text-[52px] font-bold md:w-[600px] max-w-[600px] text-left text-[#0A100B] ${outfit.className}`}
         >
           Terms & conditions
         </h1>
-        <p className="text-center md:text-left mt-10 px-4 md:px-0 text-base max-w-[600px] mb-10">
+        <p className="text-center md:text-left mt-10 px-4 md:px-0 text-base max-w-[600px]">
           Marble Legal Solutions Inc provides self-help services at your
           specific direction. We are not a law firm or a substitute for an
           attorney or law firm. Communications between you and Marble Legal
